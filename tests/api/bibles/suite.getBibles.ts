4ec81033b7bb931225cab9cd1cab9de54fcdc6fe
melinoid <melinoid@gmail.com>
2024-10-15 14:48:37 +0500

    tidy up the bibles suite

--- tests/api/bibles/suite.getBibles.ts
+++ tests/api/bibles/suite.getBibles.ts
@@ -1,36 +1,80 @@
 import { APIResponse } from '@playwright/test';
 import { test } from '../../../utils/fixtures';
 import * as expBody from './responses.json';
+import { bible } from '../../../utils/config';
 
 const apiPath = '/v1/bibles';
 let response: APIResponse;
 
 test.describe('/v1/bibles', async () => {
-  test('200 code w/o params', async ({ request, helper }) => {
+  test('200 code (w/o params)', async ({ request, helper }) => {
     await test.step('Send request', async () => {
       response = await request.get(apiPath, {});
     });
     await test.step('Compare status code', async () => {
       helper.compareStatusCode(response.status(), 200);
     });
-    await test.step('Compare response object', async () => {
-      const array1 = Object.keys((await response.json())['data'][4]);
-      const array2 = Object.keys(expBody['200wop']);
-
-      if (!(array1.length === array2.length && array1.every((value, index) => value === array2[index]))) {
-        throw Error('Something went wrong, check the request response.');
-      }
+    // The responce is too big, so let's check just the model.
+    await test.step('Compare response model', async () => {
+      helper.compareObjectsKeys(expBody['200wop'], await response.json());
     });
   });
 
-  test('200 code with all params', async ({ request, helper }) => {
+  test('200 code (params w/o id)', async ({ request, helper }, testInfo) => {
     await test.step('Send request', async () => {
       response = await request.get(apiPath, {
         params: {
           language: 'eng',
-          abbreviation: expBody['200wap']['data']['0']['abbreviation'],
-          name: expBody['200wap']['data']['0']['name'],
-          ids: expBody['200wap']['data']['0']['id'],
+          abbreviation: 'WEB',
+          name: 'British',
+        },
+      });
+    });
+    await test.step('Compare status code', async () => {
+      helper.compareStatusCode(response.status(), 200);
+    });
+    await test.step('Compare response text', async () => {
+      if (testInfo.retry == 0) {
+        helper.compareResponseText(expBody['200woid'], await response.json());
+      } else {
+        // Тhe response is too big, it may change over time, so we check the model on first retry.
+        console.log(`Test data in test: "${testInfo.titlePath[1]} ${testInfo.titlePath[2]}" is expired.`);
+        helper.compareObjectsKeys(expBody['200woid'], await response.json());
+      }
+    });
+  });
+
+  test('200 code (multiple ids)', async ({ request, helper }, testInfo) => {
+    await test.step('Send request', async () => {
+      response = await request.get(apiPath, {
+        params: {
+          ids: `${bible.book.parallelId},${bible.id}`,
+          'include-full-details': false,
+        },
+      });
+    });
+    await test.step('Compare status code', async () => {
+      helper.compareStatusCode(response.status(), 200);
+    });
+    await test.step('Compare response text', async () => {
+      if (testInfo.retry == 0) {
+        helper.compareResponseText(expBody['200mid'], await response.json());
+      } else {
+        // Тhe response is too big, it may change over time, so we check the model on first retry.
+        console.log(`Test data in test: "${testInfo.titlePath[1]} ${testInfo.titlePath[2]}" is expired.`);
+        helper.compareObjectsKeys(expBody['200mid'], await response.json());
+      }
+    });
+  });
+
+  test('200 code (all params)', async ({ request, helper }, testInfo) => {
+    await test.step('Send request', async () => {
+      response = await request.get(apiPath, {
+        params: {
+          language: 'eng',
+          abbreviation: 'BSB',
+          name: 'Berean Standard Bible',
+          ids: bible.id,
           'include-full-details': true,
         },
       });
@@ -39,10 +83,13 @@ test.describe('/v1/bibles', async () => {
       helper.compareStatusCode(response.status(), 200);
     });
     await test.step('Compare response text', async () => {
-      response = await response.json();
-      response['data'][0]['updatedAt'] = '';
-
-      helper.compareResponseText(expBody['200wap'], response);
+      if (testInfo.retry == 0) {
+        helper.compareResponseText(expBody['200wap'], await response.json());
+      } else {
+        // Тhe response is too big, it may change over time, so we check the model on first retry.
+        console.log(`Test data in test: "${testInfo.titlePath[1]} ${testInfo.titlePath[2]}" is expired.`);
+        helper.compareObjectsKeys(expBody['200wap'], await response.json());
+      }
     });
   });
 
